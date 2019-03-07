4fccc39a650eb201196847f2579b0b19323147cd
Jordan6129 <43240502+Jordan6129@users.noreply.github.com>
2019-03-07 09:28:10 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$("button").click(function(){ // when the button is clicked the function is activated //
+
+    var career = $(".career").val(); // declaration of the variable "career" //
+    
+    var hours = parseInt($("#hours").val()); // declaring the variable "hours" and giving it the value of the input id "hours"//
+    
+    var profession = $(".profession").val; // declaration of the variale "profession" //
+    
+    var salary = parseInt(salary)*26;
+    $("p").text("By the year 2045 you would have made"+salary+" dollars.");
+});
